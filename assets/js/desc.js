b0033ec7b48b9c22ae4be8abcf0430f92f83e2ab
Roberto Vicario <rvicariodev@gmail.com>
2024-04-16 20:58:32 +0200

    updates

--- assets/js/desc.js
+++
@@ -1,5 +0,0 @@
-const desc = document.getElementById('desc');
-desc.addEventListener('input', () => {
-        desc.style.height = 'auto';
-        desc.style.height = `${desc.scrollHeight}px`;
-});
